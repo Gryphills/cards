ee077b110b2d8c890bec3e76f58ae9b3b7db0f80
Gryphills <87514349+Gryphills@users.noreply.github.com>
2025-05-13 15:29:51 -0700

    Update cards.js

--- cards.js
+++ cards.js
@@ -1,4 +1,16 @@
 
+const scipiosLook = {
+  title: "Scipios Looking",
+  gif: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/66d32d06-14be-4636-b25f-95141f65a139/dikkxh4-7d667e9c-1897-4a11-a978-8da4b77fd9a3.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzY2ZDMyZDA2LTE0YmUtNDYzNi1iMjVmLTk1MTQxZjY1YTEzOVwvZGlra3hoNC03ZDY2N2U5Yy0xODk3LTRhMTEtYTk3OC04ZGE0Yjc3ZmQ5YTMuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.kPoXqQLC3Qk_78kQpHbdNKEnvjP--BFGGI1eAu8K510',
+  video: 'https://syncsketch.com/sketch/NGQ1YzMzYzk0/#/30485679/31698338',
+  thumbnail: 'https://cdn.discordapp.com/attachments/1195987621218898040/1371964056298323968/scipio-0019.png?ex=68250c98&is=6823bb18&hm=0dcd37eba9d81fd008f69ebe7dea51598557cf179780bb027187614f34156fff&',
+  sectionLog: [
+    ['Overall Bonuses', 'abstract'],
+    ['5-19', 'fullBody', 'animationPrinciples', 'offsetDetails', 'traditionalf'] //7 drawings w 6 sections body - 84 minimum mp
+  ]
+}
+
+
 
 
 
@@ -10,13 +22,21 @@ const tritTrot = {
   thumbnail: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/66d32d06-14be-4636-b25f-95141f65a139/dh334u7-60a4bd21-69b4-489f-9163-28fe8e2984f7.gif/v1/fill/w_622,h_350,q_85,strp/trit_trottin__along___animation_by_gryphonillustrations_dh334u7-350t.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTA4MCIsInBhdGgiOiJcL2ZcLzY2ZDMyZDA2LTE0YmUtNDYzNi1iMjVmLTk1MTQxZjY1YTEzOVwvZGgzMzR1Ny02MGE0YmQyMS02OWI0LTQ4OWYtOTE2My0yOGZlOGUyOTg0ZjcuZ2lmIiwid2lkdGgiOiI8PTE5MjAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.DyxIj7r-18_wPTQHrlalKGB6dJAwcJ9oxMbFfbwk3dU',
   sectionLog: [
     ['Overall Bonuses', 'pAndE', 'bgSimple'],
-    ['1-11', 'fullBody', 'offsetDetails', 'traditionalf', 'shading', 'magic'],
-    ['11-22', 'fullBody', 'offsetDetails', 'traditionalf', 'shading', 'magic']
+    ['1-11', 'fullBody', 'animationPrinciples', 'traditionalf', 'shading', 'magic'],
+    ['11-22', 'fullBody', 'animationPrinciples', 'traditionalf', 'shading', 'magic']
+  ]
+}
+
+const minecraftRun = {
+  title: 'Minecraft Running',
+  sectionLog: [
+    ['frames 1-5', 'fullBody', 'animationPrinciples'],
+    ['frames 5-10', 'fullBody', 'animationPrinciples']
   ]
 }
 
 const selkieSwim = {
-  title: `Selkie's Swim`,
+  title: `Selkie's Swim Coac`,
   video: 'https://syncsketch.com/sketch/MjYyMTkxZGI5/#/29371608/30526385',
   gif: 'https://rexalia.world/storage/galeria/28/frrjDsx1CABXrE094YVcQROCSPsOWK2qibnFpCks.gif',
   thumbnail: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/32deac1a-b3f1-4c9b-9a0b-475435f09fee/dh7cgia-867b788f-fad4-4095-8f2b-226761abd2c7.gif/v1/fill/w_219,h_350,q_85,strp/_anim__at_home_in_the_deep_by_visceralluster_dh7cgia-350t.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTIwMCIsInBhdGgiOiJcL2ZcLzMyZGVhYzFhLWIzZjEtNGM5Yi05YTBiLTQ3NTQzNWYwOWZlZVwvZGg3Y2dpYS04NjdiNzg4Zi1mYWQ0LTQwOTUtOGYyYi0yMjY3NjFhYmQyYzcuZ2lmIiwid2lkdGgiOiI8PTc1MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.PHtBitfM8xMUMfPbtD3uYYe62Xiwf_01x4dvYK8FNrA',
@@ -28,6 +48,21 @@ const selkieSwim = {
   ]
 }
 
+const flowerCrown = {
+  title: "Flower Crown Fun",
+  sectionLog: [
+    ['Overall Bonuses', 'pAndE'],
+    ['1-2', 'partialBody', 'animationPrinciples', 'accessories', 'shading'],
+    ['2-3', 'halfBody', 'animationPrinciples', 'accessories', 'shading'],
+    ['3-4', 'halfBody', 'animationPrinciples', 'accessories', 'shading']
+  ],
+  rexLink: 'https://rexalia.world/rex/1843',
+  video: 'https://wixmp-ed30a86b8c4ca887773594c2.wixmp.com/v/mp4/66d32d06-14be-4636-b25f-95141f65a139/dho6i92-9d585a31-4e62-43b9-baec-14649c9f38bd.VideoQualities.res_1080p.cd5fdeb1f78a48869f03a16a4e08afb7.mp4',
+  thumbnail: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/66d32d06-14be-4636-b25f-95141f65a139/dho6i92-bdd2204f-db03-42a1-893a-146fb860c19c.jpg/v1/fill/w_1069,h_748,q_70,strp/flower_crown_crafts___animation_by_gryphonillustrations_dho6i92-pre.jpg'
+}
+
+
+
 const foxJump = {
   title: 'Snowy Snoot Jump',
   gif: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/66d32d06-14be-4636-b25f-95141f65a139/djr4q9l-81060453-72d9-432e-975f-54d6cf20480e.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzY2ZDMyZDA2LTE0YmUtNDYzNi1iMjVmLTk1MTQxZjY1YTEzOVwvZGpyNHE5bC04MTA2MDQ1My03MmQ5LTQzMmUtOTc1Zi01NGQ2Y2YyMDQ4MGUuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.g4YYP_kZGyB4NQ3gHgGf0l1rhFkIpfsWPQikjuiQ6uU',
@@ -36,9 +71,40 @@ const foxJump = {
   sectionLog: [
     ['Overall Bonuses', 'abstract'],
     ['21-31', 'fullBody', 'animationPrinciples', 'offsetDetails', 'traditionalf'],
-    ['31-41', 'fullBody', 'animationPrinciples', 'offsetDetails', 'traditionalf'],
-    ['41-53', 'fullBody', 'animationPrinciples', 'offsetDetails', 'traditionalf'],
-    ['55-69', 'fullBody', 'animationPrinciples', 'offsetDetails', 'traditionalf'],
+    ['31-41', 'fullBody', 'animationPrinciples',  'traditionalf'],
+    ['41-53', 'fullBody', 'animationPrinciples',  'traditionalf'],
+    ['55-69', 'fullBody', 'animationPrinciples', 'traditionalf'],
+  ]
+}
+
+const hyllinFight = {
+  gif: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/c1ae54db-959b-4f49-aadc-f682f0d0725e/djqpjr3-5fd09a1f-e4a4-4917-b201-97e4442f9136.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2MxYWU1NGRiLTk1OWItNGY0OS1hYWRjLWY2ODJmMGQwNzI1ZVwvZGpxcGpyMy01ZmQwOWExZi1lNGE0LTQ5MTctYjIwMS05N2U0NDQyZjkxMzYuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.03TPhmYUu60drWHjw0j4devwE88M8tTYj4OVQDbOGP4',
+  title: "Fight!!!",
+  video: 'l',
+  rexLink: 'l',
+  sectionLog: [
+    ['Overall Bonuses', 'pAndE', 'bgSimple'],
+    ['1-3', 'fullBody', 'animationPrinciples', 'shading', 'traditionalf', 'magic', 'outfit'],
+    ['3-5', 'fullBody', 'animationPrinciples', 'shading', 'traditionalf', 'magic', 'outfit']
+  ],
+  thumbnail: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/c1ae54db-959b-4f49-aadc-f682f0d0725e/djq5q15-45668a4e-4336-46e0-a5bb-5e24cc40d01e.png/v1/fill/w_1081,h_739,q_70,strp/timeline_1_0005_by_hyllin_djq5q15-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9ODc2IiwicGF0aCI6IlwvZlwvYzFhZTU0ZGItOTU5Yi00ZjQ5LWFhZGMtZjY4MmYwZDA3MjVlXC9kanE1cTE1LTQ1NjY4YTRlLTQzMzYtNDZlMC1hNWJiLTVlMjRjYzQwZDAxZS5wbmciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.z8IhFd7quLZKDJ7_zgUrdvQSSWYuDhS6OgRFzp0-kIc'
+
+}
+
+
+const astra = {
+  title: "Astra-talking",
+  video: 'https://syncsketch.com/sketch/c6F7u0YiY5lf/#/34512717/35944540',
+  thumbnail: 'https://media.discordapp.net/attachments/1195987621218898040/1371958755708899409/image.png?ex=682507a9&is=6823b629&hm=e45d50caf33e9c786b70a13b7487c9ec2097b20f8e286c3937e2bcf5bd3a0a6f&=&format=webp&quality=lossless',
+  rexLink: 'https://rexalia.world/rex/2657',
+  gif: 'nope',
+  sectionLog: [ [ '1-13',  'halfBody',  'animationPrinciples',  'traditionalh',  'offsetDetails',  'jewellery' ],
+  ['13-29','halfBody','animationPrinciples',  'traditionalh',  'offsetDetails',  'jewellery' ],
+  ['29-39',  'halfBody',  'animationPrinciples',  'traditionalh',  'offsetDetails',  'jewellery' ],
+  ['39-51',  'halfBody',  'animationPrinciples',  'traditionalh',  'offsetDetails',  'jewellery' ] ,
+  ['51-59',  'halfBody',  'animationPrinciples',  'traditionalh',  'offsetDetails',  'jewellery' ] ,
+  ['59-69',  'halfBody',  'animationPrinciples',  'traditionalh',  'offsetDetails',  'jewellery'],
+  ['59-87',  'halfBody',  'animationPrinciples',  'traditionalh',  'offsetDetails',  'jewellery']
   ]
 }
 
@@ -50,12 +116,67 @@ const sirius = {
 
 const starburst = {
   title: "FightyStar",
-  sectionLog: [['Overall Bonuses', 'abstract'], ['1-1', 'halfBody', 'animationPrinciples', 'accessories', 'magic', 'shading'], 
-  ['2-2', 'halfBody', 'animationPrinciples', 'offsetDetails', 'accessories', 'magic', 'shading']],
+  sectionLog: [
+    ['Overall Bonuses', 'abstract'], 
+  ['15-31', 'halfBody', 'animationPrinciples', 'accessories', 'magic', 'shading'], 
+  ['39-59', 'halfBody', 'animationPrinciples', 'traditionalh', 'accessories', 'magic', 'shading']],
   thumbnail: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/66d32d06-14be-4636-b25f-95141f65a139/dh6lsa9-66008e5e-bab1-44a0-8572-51a39d11bee9.gif/v1/fill/w_622,h_350,q_85,strp/you_sure_you_want_a_fight____animation_by_gryphonillustrations_dh6lsa9-350t.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTA4MCIsInBhdGgiOiJcL2ZcLzY2ZDMyZDA2LTE0YmUtNDYzNi1iMjVmLTk1MTQxZjY1YTEzOVwvZGg2bHNhOS02NjAwOGU1ZS1iYWIxLTQ0YTAtODU3Mi01MWEzOWQxMWJlZTkuZ2lmIiwid2lkdGgiOiI8PTE5MjAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.y3BvMU0a4sG9JqsPrE3_5iQG9r85NRGUnoFHgdzjcXU",
   gif: `https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/66d32d06-14be-4636-b25f-95141f65a139/dh6lsa9-66008e5e-bab1-44a0-8572-51a39d11bee9.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzY2ZDMyZDA2LTE0YmUtNDYzNi1iMjVmLTk1MTQxZjY1YTEzOVwvZGg2bHNhOS02NjAwOGU1ZS1iYWIxLTQ0YTAtODU3Mi01MWEzOWQxMWJlZTkuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.ARIXJQm3imbryR_wMQAUvU-tJqlVU-rzwk-i-yzDRio`,
   video: 'https://syncsketch.com/sketch/NDcyNzJmZDVm/#/29008830/30145593',
   rexLink: 'https://rexalia.world/rex/1513'
 }
 
-document.getElementById('all-logs').innerHTML = `<p> testing bc idk what is up rn </p>`;
+const coac = {
+  title: "coacChaos",
+  sectionLog: [ [ 'Overall Bonuses', 'pAndE', 'bgSimple' ],
+  [ '21-36', 'halfBody', 'animationPrinciples', 'shading' ],
+  [ '36-55', 'fullBody', 'animationPrinciples', 'shading' ],
+  [ '55-79', 'fullBody', 'animationPrinciples', 'shading' ],
+  [ '85-95', 'fullBody', 'animationPrinciples', 'shading' ],
+  [ '95-101', 'fullBody', 'animationPrinciples', 'shading' ],
+  [ '107-113',  'fullBody',  'animationPrinciples', 'shading',  'magic' ],
+  ['113-121',    'halfBody',    'animationPrinciples',    'shading',    'magic',]],
+    importLink: "link",
+    videoLink: "vid",
+    thumbnail: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/66d32d06-14be-4636-b25f-95141f65a139/dhbf9qs-5e656881-b711-49bf-96ef-bfdc35bc2de4.jpg/v1/fill/w_1192,h_670,q_70,strp/the_stag___animation_by_gryphonillustrations_dhbf9qs-pre.jpg"
+}
+
+const coac2 = {
+  title: "coacChaos Scene 2",
+  sectionLog: [ [ 'Overall Bonuses', 'pAndE', 'bgSimple' ],
+  [ '131-135',     'halfBody',    'animationPrinciples',    'shading',    'magic' ],
+  [ '135-145',    'fullBody',    'animationPrinciples',    'shading',    'magic' ],
+  [ '145-161',    'fullBody',    'animationPrinciples',    'shading',    'magic' ],
+  [ '161-179', 'fullBody', 'animationPrinciples', 'shading' ] ]
+
+}
+
+
+
+const bkanim = {
+  title: "Where am I?",
+  rexLink: 'link',
+  video: 'link',
+  thumbnail: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/d5874dce-fbf8-4a24-9ac6-7ff8037c25dc/digv9gz-1ea3ca16-0886-45b4-a56b-1a371c4b15ec.jpg/v1/fill/w_1039,h_769,q_70,strp/faces_of_anxiety_by_babykitten123_digv9gz-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2Q1ODc0ZGNlLWZiZjgtNGEyNC05YWM2LTdmZjgwMzdjMjVkY1wvZGlndjlnei0xZWEzY2ExNi0wODg2LTQ1YjQtYTU2Yi0xYTM3MWM0YjE1ZWMuanBnIiwiaGVpZ2h0IjoiPD0xNDIxIiwid2lkdGgiOiI8PTE5MjAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uud2F0ZXJtYXJrIl0sIndtayI6eyJwYXRoIjoiXC93bVwvZDU4NzRkY2UtZmJmOC00YTI0LTlhYzYtN2ZmODAzN2MyNWRjXC9iYWJ5a2l0dGVuMTIzLTQucG5nIiwib3BhY2l0eSI6OTUsInByb3BvcnRpb25zIjowLjQ1LCJncmF2aXR5IjoiY2VudGVyIn19.ycV-RJ-Ow82pL3tYrT9KvwvhL7BB0GSQrlbupsNi2nw',
+  sectionLog: [
+    ['movement-1', 'fullBody', 'animationPrinciples', 'traditionalf'],
+    ['movement-2', 'fullBody', 'animationPrinciples', 'traditionalf'],
+    ['movement-3', 'fullBody', 'animationPrinciples', 'traditionalf', 'offsetDetails']
+  ]
+}
+
+
+//////
+
+const sharkSave = {
+  title: 'Shark Save',
+  video: 'https://wixmp-ed30a86b8c4ca887773594c2.wixmp.com/v/mp4/66d32d06-14be-4636-b25f-95141f65a139/dhrez0e-bc4e7463-e482-4356-88ec-d748514caa48.VideoQualities.res_1080p.9d4603cfda884763b8cc8806c0fe4a8b.mp4',
+  thumbnail: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/66d32d06-14be-4636-b25f-95141f65a139/dhrez0e-5e5f9fc3-d4f8-4312-b4bb-fd091653824c.jpg/v1/fill/w_1192,h_670,q_70,strp/shark_save____animation_by_gryphonillustrations_dhrez0e-pre.jpg',
+  rexLink: 'https://rexalia.world/rex/2237',
+  sectionLog: [
+    ['Overall Bonuses', 'pAndE', 'bgSimple'],
+    ['fr6-7', 'fullBody', 'animationPrinciples', 'shading'],
+    ['fr7-8', 'fullBody', 'animationPrinciples', 'shading'],
+    ['fr8-9', 'fullBody', 'animationPrinciples', 'shading', 'offsetDetails']
+  ]
+}
