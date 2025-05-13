4f0745d8d9f5257c39518eccf1008771357fd84e
Gryphills <87514349+Gryphills@users.noreply.github.com>
2025-05-13 14:01:33 -0700

    Create cards.js

---
+++ cards.js
@@ -0,0 +1,59 @@
+
+
+
+
+const tritTrot = {
+  title: 'Trit Trottin Along',
+  video: 'https://syncsketch.com/sketch/ZDE2OGI2ZWQ0/#/29440984/30600240',
+  rexLink: 'https://rexalia.world/rex/2001',
+  gif: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/66d32d06-14be-4636-b25f-95141f65a139/dh334u7-60a4bd21-69b4-489f-9163-28fe8e2984f7.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzY2ZDMyZDA2LTE0YmUtNDYzNi1iMjVmLTk1MTQxZjY1YTEzOVwvZGgzMzR1Ny02MGE0YmQyMS02OWI0LTQ4OWYtOTE2My0yOGZlOGUyOTg0ZjcuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.AIeiLR6Aj7q3KdUTCF19BkMZcJ3Lyk0nTKcIUI7rIZI',
+  thumbnail: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/66d32d06-14be-4636-b25f-95141f65a139/dh334u7-60a4bd21-69b4-489f-9163-28fe8e2984f7.gif/v1/fill/w_622,h_350,q_85,strp/trit_trottin__along___animation_by_gryphonillustrations_dh334u7-350t.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTA4MCIsInBhdGgiOiJcL2ZcLzY2ZDMyZDA2LTE0YmUtNDYzNi1iMjVmLTk1MTQxZjY1YTEzOVwvZGgzMzR1Ny02MGE0YmQyMS02OWI0LTQ4OWYtOTE2My0yOGZlOGUyOTg0ZjcuZ2lmIiwid2lkdGgiOiI8PTE5MjAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.DyxIj7r-18_wPTQHrlalKGB6dJAwcJ9oxMbFfbwk3dU',
+  sectionLog: [
+    ['Overall Bonuses', 'pAndE', 'bgSimple'],
+    ['1-11', 'fullBody', 'offsetDetails', 'traditionalf', 'shading', 'magic'],
+    ['11-22', 'fullBody', 'offsetDetails', 'traditionalf', 'shading', 'magic']
+  ]
+}
+
+const selkieSwim = {
+  title: `Selkie's Swim`,
+  video: 'https://syncsketch.com/sketch/MjYyMTkxZGI5/#/29371608/30526385',
+  gif: 'https://rexalia.world/storage/galeria/28/frrjDsx1CABXrE094YVcQROCSPsOWK2qibnFpCks.gif',
+  thumbnail: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/32deac1a-b3f1-4c9b-9a0b-475435f09fee/dh7cgia-867b788f-fad4-4095-8f2b-226761abd2c7.gif/v1/fill/w_219,h_350,q_85,strp/_anim__at_home_in_the_deep_by_visceralluster_dh7cgia-350t.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTIwMCIsInBhdGgiOiJcL2ZcLzMyZGVhYzFhLWIzZjEtNGM5Yi05YTBiLTQ3NTQzNWYwOWZlZVwvZGg3Y2dpYS04NjdiNzg4Zi1mYWQ0LTQwOTUtOGYyYi0yMjY3NjFhYmQyYzcuZ2lmIiwid2lkdGgiOiI8PTc1MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.PHtBitfM8xMUMfPbtD3uYYe62Xiwf_01x4dvYK8FNrA',
+  rexLink: 'https://rexalia.world/rex/28',
+  sectionLog: [
+    ['Overall Bonuses', 'pAndE', 'bgComplex'],
+    ['9-21', 'fullBody', 'offsetDetails', 'animationPrinciples', 'traditionalf', 'shading', 'magic'],
+    ['21-36', 'fullBody', 'offsetDetails', 'animationPrinciples', 'traditionalf', 'shading', 'magic']
+  ]
+}
+
+const foxJump = {
+  title: 'Snowy Snoot Jump',
+  gif: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/66d32d06-14be-4636-b25f-95141f65a139/djr4q9l-81060453-72d9-432e-975f-54d6cf20480e.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzY2ZDMyZDA2LTE0YmUtNDYzNi1iMjVmLTk1MTQxZjY1YTEzOVwvZGpyNHE5bC04MTA2MDQ1My03MmQ5LTQzMmUtOTc1Zi01NGQ2Y2YyMDQ4MGUuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.g4YYP_kZGyB4NQ3gHgGf0l1rhFkIpfsWPQikjuiQ6uU',
+  video: 'https://syncsketch.com/sketch/YzU4MGUyM2Fl/#/30691037/31913413',
+  thumbnail: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/66d32d06-14be-4636-b25f-95141f65a139/djr4q9l-81060453-72d9-432e-975f-54d6cf20480e.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzY2ZDMyZDA2LTE0YmUtNDYzNi1iMjVmLTk1MTQxZjY1YTEzOVwvZGpyNHE5bC04MTA2MDQ1My03MmQ5LTQzMmUtOTc1Zi01NGQ2Y2YyMDQ4MGUuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.g4YYP_kZGyB4NQ3gHgGf0l1rhFkIpfsWPQikjuiQ6uU',
+  sectionLog: [
+    ['Overall Bonuses', 'abstract'],
+    ['21-31', 'fullBody', 'animationPrinciples', 'offsetDetails', 'traditionalf'],
+    ['31-41', 'fullBody', 'animationPrinciples', 'offsetDetails', 'traditionalf'],
+    ['41-53', 'fullBody', 'animationPrinciples', 'offsetDetails', 'traditionalf'],
+    ['55-69', 'fullBody', 'animationPrinciples', 'offsetDetails', 'traditionalf'],
+  ]
+}
+
+const sirius = {
+  title: "Sirius Squats",
+  sectionLog: [  [ 'x-x', 'fullBody']
+  ]
+}
+
+const starburst = {
+  title: "FightyStar",
+  sectionLog: [['Overall Bonuses', 'abstract'], ['1-1', 'halfBody', 'animationPrinciples', 'accessories', 'magic', 'shading'], 
+  ['2-2', 'halfBody', 'animationPrinciples', 'offsetDetails', 'accessories', 'magic', 'shading']],
+  thumbnail: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/66d32d06-14be-4636-b25f-95141f65a139/dh6lsa9-66008e5e-bab1-44a0-8572-51a39d11bee9.gif/v1/fill/w_622,h_350,q_85,strp/you_sure_you_want_a_fight____animation_by_gryphonillustrations_dh6lsa9-350t.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTA4MCIsInBhdGgiOiJcL2ZcLzY2ZDMyZDA2LTE0YmUtNDYzNi1iMjVmLTk1MTQxZjY1YTEzOVwvZGg2bHNhOS02NjAwOGU1ZS1iYWIxLTQ0YTAtODU3Mi01MWEzOWQxMWJlZTkuZ2lmIiwid2lkdGgiOiI8PTE5MjAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.y3BvMU0a4sG9JqsPrE3_5iQG9r85NRGUnoFHgdzjcXU",
+  gif: `https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/66d32d06-14be-4636-b25f-95141f65a139/dh6lsa9-66008e5e-bab1-44a0-8572-51a39d11bee9.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzY2ZDMyZDA2LTE0YmUtNDYzNi1iMjVmLTk1MTQxZjY1YTEzOVwvZGg2bHNhOS02NjAwOGU1ZS1iYWIxLTQ0YTAtODU3Mi01MWEzOWQxMWJlZTkuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.ARIXJQm3imbryR_wMQAUvU-tJqlVU-rzwk-i-yzDRio`,
+  video: 'https://syncsketch.com/sketch/NDcyNzJmZDVm/#/29008830/30145593',
+  rexLink: 'https://rexalia.world/rex/1513'
+}
