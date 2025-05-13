051513d6f4c4c1d7a2d142f680a5f80055a33712
Gryphills <87514349+Gryphills@users.noreply.github.com>
2025-05-13 15:32:05 -0700

    Update script.js

--- script.js
+++ script.js
@@ -32,63 +32,6 @@ const bonusesObject = {
   
 const bgStuff = ["abstract", "pOrE", "pAndE"];
 
-const bkanim = {
-  title: "Where am I?",
-  rexLink: 'link',
-  video: 'link',
-  thumbnail: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/d5874dce-fbf8-4a24-9ac6-7ff8037c25dc/digv9gz-1ea3ca16-0886-45b4-a56b-1a371c4b15ec.jpg/v1/fill/w_1039,h_769,q_70,strp/faces_of_anxiety_by_babykitten123_digv9gz-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2Q1ODc0ZGNlLWZiZjgtNGEyNC05YWM2LTdmZjgwMzdjMjVkY1wvZGlndjlnei0xZWEzY2ExNi0wODg2LTQ1YjQtYTU2Yi0xYTM3MWM0YjE1ZWMuanBnIiwiaGVpZ2h0IjoiPD0xNDIxIiwid2lkdGgiOiI8PTE5MjAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uud2F0ZXJtYXJrIl0sIndtayI6eyJwYXRoIjoiXC93bVwvZDU4NzRkY2UtZmJmOC00YTI0LTlhYzYtN2ZmODAzN2MyNWRjXC9iYWJ5a2l0dGVuMTIzLTQucG5nIiwib3BhY2l0eSI6OTUsInByb3BvcnRpb25zIjowLjQ1LCJncmF2aXR5IjoiY2VudGVyIn19.ycV-RJ-Ow82pL3tYrT9KvwvhL7BB0GSQrlbupsNi2nw',
-  sectionLog: [
-    ['movement-1', 'fullBody', 'animationPrinciples', 'traditionalf'],
-    ['movement-2', 'fullBody', 'animationPrinciples', 'traditionalf'],
-    ['movement-3', 'fullBody', 'animationPrinciples', 'traditionalf', 'offsetDetails']
-  ]
-}
-
-
-const flowerCrown = {
-  title: "Flower Crown Fun",
-  sectionLog: [
-    ['Overall Bonuses', 'pAndE'],
-    ['1-2', 'partialBody', 'animationPrinciples', 'accessories', 'shading'],
-    ['2-3', 'halfBody', 'animationPrinciples', 'accessories', 'shading'],
-    ['3-4', 'halfBody', 'animationPrinciples', 'accessories', 'shading']
-  ],
-  rexLink: 'https://rexalia.world/rex/1843',
-  video: 'https://wixmp-ed30a86b8c4ca887773594c2.wixmp.com/v/mp4/66d32d06-14be-4636-b25f-95141f65a139/dho6i92-9d585a31-4e62-43b9-baec-14649c9f38bd.VideoQualities.res_1080p.cd5fdeb1f78a48869f03a16a4e08afb7.mp4',
-  thumbnail: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/66d32d06-14be-4636-b25f-95141f65a139/dho6i92-bdd2204f-db03-42a1-893a-146fb860c19c.jpg/v1/fill/w_1069,h_748,q_70,strp/flower_crown_crafts___animation_by_gryphonillustrations_dho6i92-pre.jpg'
-}
-
-const sharkSave = {
-  title: 'Shark Save',
-  video: 'https://wixmp-ed30a86b8c4ca887773594c2.wixmp.com/v/mp4/66d32d06-14be-4636-b25f-95141f65a139/dhrez0e-bc4e7463-e482-4356-88ec-d748514caa48.VideoQualities.res_1080p.9d4603cfda884763b8cc8806c0fe4a8b.mp4',
-  thumbnail: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/66d32d06-14be-4636-b25f-95141f65a139/dhrez0e-5e5f9fc3-d4f8-4312-b4bb-fd091653824c.jpg/v1/fill/w_1192,h_670,q_70,strp/shark_save____animation_by_gryphonillustrations_dhrez0e-pre.jpg',
-  rexLink: 'https://rexalia.world/rex/2237',
-  sectionLog: [
-    ['Overall Bonuses', 'pAndE', 'bgSimple'],
-    ['fr6-7', 'fullBody', 'animationPrinciples', 'shading'],
-    ['fr7-8', 'fullBody', 'animationPrinciples', 'shading'],
-    ['fr8-9', 'fullBody', 'animationPrinciples', 'shading', 'offsetDetails']
-  ]
-}
-
-const scipiosLook = {
-  title: "scipiosLook",
-  video: 'temp',
-  thumbnail: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/66d32d06-14be-4636-b25f-95141f65a139/dho5tik-ccc4e5de-ca1f-4ac8-9c33-4ac0616999fc.gif/v1/fill/w_425,h_270,q_85,strp/scraps_test_by_gryphonillustrations_dho5tik-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MjcwIiwicGF0aCI6IlwvZlwvNjZkMzJkMDYtMTRiZS00NjM2LWIyNWYtOTUxNDFmNjVhMTM5XC9kaG81dGlrLWNjYzRlNWRlLWNhMWYtNGFjOC05YzMzLTRhYzA2MTY5OTlmYy5naWYiLCJ3aWR0aCI6Ijw9NDI1In1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.pACdT6FfHC10OCI8uc6T8kYjy097ZbigrXjLvMGJo2E',
-  sectionLog: [
-    ['Overall Bonuses', 'abstract'],
-    ['1-10', 'fullBody', 'animationPrinciples', 'offsetDetails', 'traditionalf']
-  ]
-}
-
-const minecraftRun = {
-  title: 'minecraft Run',
-  sectionLog: [
-    ['frames 1-5', 'fullBody', 'animationPrinciples'],
-    ['frames 5-10', 'fullBody', 'animationPrinciples']
-  ]
-}
-
 
 function start() {
 
