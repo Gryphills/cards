32a0723475d7c9c9b61ce26a6087e8ae0fbe77f4
Gryphills <87514349+Gryphills@users.noreply.github.com>
2025-05-14 10:32:35 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
-const siriuss = {
-  title: "Sirius Squats",
-  sectionLog: [  [ 'x-x', 'fullBody']
-  ]
-}
-
 
 const bonusesObject = {
 
-  partialBody : [ 2, "Partial Body (1-2 sections)" ],
-  halfBody  : [ 7, "Half Body (3-5 sections)" ],
+  partialBody : [ 3, "Partial Body (1-2 sections)" ],
+  halfBody  : [ 8, "Half Body (3-5 sections)" ],
   fullBody  : [15, "Full Body (6-7 sections)"],
 
-  animationPrinciples  : [10, "Animation Principles"],
-  offsetDetails  : [10, "Offset or Complex Details"],
+  animationPrinciples  : [2, "Animation Principles"],
+  offsetDetails  : [0, "Complex Details"],
+  trad3: [2, "3+ handDrawn frames inbetween keys"],
 
-  shading  : [3, "Shaded"],
+  shading  : [0.5, "Shaded"],
   traditional  : [4, "x body - Traditional Bonus"],
 
   magic  : [4, "Magic"],
@@ -22,10 +17,10 @@ const bonusesObject = {
   accessories  : [4, "Accessories"],
   outfit  : [4, "Clothing"],
 
-  otherRex  : [2, "(Static) Other Player Rex"],
+  /*otherRex  : [2, "(Static) Other Player Rex"],
   otherRexAnim  : [4, "Animated Other Player Rex"],
   companion  : [1, "(Static) Companion"],
-  companionAnim  : [2, "Animated Companion"],
+  companionAnim  : [2, "Animated Companion"],*/
 
   bgSimple  : [4, "Simple Animated BG (stacks)"],
   bgComplex  : [10, "Complex Animated BG (stacks)"],
@@ -38,9 +33,9 @@ const bonusesObject = {
   
 const bgStuff = ["abstract", "pOrE", "pAndE"];
 
+const multiplied = ['animationPrinciples','offsetDetails', 'trad3', 'shading',  'traditional']
 
 function start() {
-
   let bonusKeys = Object.getOwnPropertyNames(bonusesObject);
   for (let i=0; i<bgStuff.length; i++) {
     let index = bonusKeys.indexOf(bgStuff[i]);
@@ -65,19 +60,36 @@ function start() {
 }
 
 function writeMPlogStuff(bonus) { //start it
-  if (bonus.includes('traditional')) {
-    let bodyArray = ['p', 'h', 'f'];    
+  
+  if (multiplied.includes(bonus)) {
+    let bodyArray = ['p', 'h', 'f'];
     for (let b=0; b<bodyArray.length; b++) {
       let type = bodyArray[b]
       if (type == "p") type = "partialBody";
       if (type == "h") type = "halfBody";
       if (type == "f") type = "fullBody";
+      let bonusAmount = (bonusesObject[type][0] * bonusesObject[bonus][0]) - bonusesObject[type][0]
+
+      writeBonuses(bonus + bodyArray[b], bonusAmount)
+    }
+  }
+  /*
+  if (bonus.includes('traditional')) {
+    let bodyArray = ['p', 'h', 'f'];
+    for (let b=0; b<bodyArray.length; b++) {
+     // console.log("yeah??")
+      let type = bodyArray[b]
+      //allBonusInstances = document.getElementsByClassName(type);
+      if (type == "p") type = "partialBody";
+      if (type == "h") type = "halfBody";
+      if (type == "f") type = "fullBody";
+        //console.log(type + "is the type???")
       let bonusAmount = (bonusesObject[type][0] * bonusesObject.traditional[0]) - bonusesObject[type][0]
 
       writeBonuses('traditional' + bodyArray[b], bonusAmount)
-    }
-  } else {
-    bonusAmount = bonusesObject[bonus][0].toString();
+    }*/
+  else {
+    bonusAmount = bonusesObject[bonus][0];
     writeBonuses(bonus, bonusAmount)
   }
 }
@@ -88,7 +100,9 @@ function writeBonuses(bonusName, bonusAmount) { //private/protected
     let item = allBonusInstances.item(i);
     if (bonusName.includes('traditional')) {
       bonusName = 'traditional';
-      item.innerHTML = "+ &nbsp;" + bonusAmount + " MP &nbsp;&nbsp; Traditional Bonus (x" + bonusesObject['traditional'][0] + " Body bonus)"
+      item.innerHTML = "+ &nbsp;" + bonusAmount + " MP &nbsp;&nbsp;" +  bonusesObject[bonusName][1] + "(x" + bonusesObject[bonusName][0] + " Body bonus) <br>"
+    } else if (bonusName.includes('trad3')) {
+      item.innerHTML = "+ &nbsp;" + bonusAmount + " MP &nbsp;&nbsp;" + bonusesObject[bonusName][1] +  "(x" + bonusesObject[bonusName][0] + " Body bonus) <br>"
     } else {
       item.innerHTML = "+ &nbsp;" + bonusAmount + " MP &nbsp;&nbsp;" + bonusesObject[bonusName][1] + "<br>";
     }
@@ -119,6 +133,7 @@ function mpLog(title, rexlink, videolink, sectionsArray, thumbnail) {
     let logContainer = document.getElementById("all-logs");
     let randomNumber = Math.floor(Math.random()*11);
     let classTitle = title.split(" ")[0] + randomNumber;
+    //console.log(classTitle)
     let newLog = `<br> <div class="mpLog"> <div class="titlestuff"> <strong> ${title} <br> <a href="${rexlink}"> Rex Import </a> -- <a href="${videolink}"> Video Link</a> </strong> </div> <br>`
     newLog += `<div class="image" id="image-${classTitle}"> <img src="${thumbnail}" height="150px"> </div> <div id="button-${classTitle}"> <i>(click to show/hide card details)</i></div> <br>`
     newLog += `<div class="cardcontainer" id="cardinfo-${classTitle}" style="display:none"> `
@@ -131,7 +146,7 @@ function mpLog(title, rexlink, videolink, sectionsArray, thumbnail) {
           newLog += `<div class="sectionstart"> ${section[0]}: </div>`;
         }
         else {
-            newLog += `<div class="${section[j]} inputrows"> </div>`
+          newLog += `<div class="${section[j]} inputrows"> </div>`
           subtotal.push(section[j])
         }
       }
@@ -141,11 +156,15 @@ function mpLog(title, rexlink, videolink, sectionsArray, thumbnail) {
     allTotals.push(sectionsArray.length);
     newLog += `</div> <div class="total" id="totals-${allTotals.length - 1}"> </div> </div>`
     logContainer.innerHTML += newLog
+
     cardInfoList.push(classTitle);
+
 }
 
 
 function calculations() {
+  //console.log(allSubtotals)
+  //calculatedSubtotals = [];
   for (let i=0; i<allSubtotals.length; i++) {
     let calculated = 0;
     let group = allSubtotals[i];
@@ -153,8 +172,10 @@ function calculations() {
 
     for (let j=0; j<group.length; j++) {
       let item = group[j];
+     // console.log(item)
       
       if (item.includes("traditional")) {
+        //console.log('TRADITIONAL OK')
         let type = item.charAt(item.length - 1);
         if (type == "p") type = "partialBody";
         if (type == "h") type = "halfBody";
@@ -162,10 +183,13 @@ function calculations() {
       
         let bonusAmount = (bonusesObject[type][0] * bonusesObject.traditional[0]) - bonusesObject[type][0]
         calculated += bonusAmount;
+        //console.log('trad bonuse: ' + bonusAmount)
         currentSubtotal.innerHTML = `<i>Subtotal: ${calculated} MP</i>`
     
       }
       else {
+        //console.log(item);
+        //console.log(bonusesObject[item])
         calculated += bonusesObject[item][0];
         currentSubtotal.innerHTML = `<i>Subtotal: ${calculated} MP</i>`
     
@@ -207,30 +231,11 @@ function buttonFunctions() {
 }
 
 
-//full-body:
-mpLogObj(siriuss)
-mpLogObj(minecraftRun)
-
-mpLogObj(hyllinFight)
-mpLogObj(scipiosLook)
-mpLogObj(selkieSwim)
-mpLogObj(foxJump)
-
-mpLogObj(coac)
-mpLogObj(coac2)
-
-//half-body:
-mpLogObj(flowerCrown)
-mpLogObj(astra)
-mpLogObj(starburst)
-
-//partialbody:
-
-//slight bonuses:
 
 start();
 
 calculations();
 
 buttonFunctions();
+
 
